55da35a44d41b1cb7d4c68fb948707e261e44fb2
Rico Metzger <rico.metzger@gmail.com>
2023-04-03 10:45:44 +0200

    fixes #104 fixes #102

--- lib/bexio_api_client/sales_order_management.ex
+++ lib/bexio_api_client/sales_order_management.ex
@@ -4,6 +4,7 @@ defmodule BexioApiClient.SalesOrderManagement do
   """
 
   import BexioApiClient.Helpers
+  alias BexioApiClient.SalesOrderManagement.PositionDefault
   alias BexioApiClient.SalesOrderManagement.PositionText
   alias BexioApiClient.SalesOrderManagement.PositionSubtotal
 
@@ -58,7 +59,7 @@ defmodule BexioApiClient.SalesOrderManagement do
           document_type :: :offer | :order | :invoice,
           document_id :: pos_integer(),
           position_id :: pos_integer()
-        ) :: {:ok, [BexioApiClient.Contacts.Contact.t()]} | {:error, any()}
+        ) :: {:ok, [BexioApiClient.SalesOrderManagement.PositionSubtotal.t()]} | {:error, any()}
   def fetch_subtotal_position(
         client,
         document_type,
@@ -146,7 +147,7 @@ defmodule BexioApiClient.SalesOrderManagement do
           document_type :: :offer | :order | :invoice,
           document_id :: pos_integer(),
           position_id :: pos_integer()
-        ) :: {:ok, [BexioApiClient.Contacts.Contact.t()]} | {:error, any()}
+        ) :: {:ok, [BexioApiClient.SalesOrderManagement.PositionText.t()]} | {:error, any()}
   def fetch_text_position(
         client,
         document_type,
@@ -176,6 +177,112 @@ defmodule BexioApiClient.SalesOrderManagement do
       id: id,
       text: text,
       show_pos_nr: show_pos_nr,
+      pos: pos,
+      internal_pos: internal_pos,
+      optional?: optional?,
+      parent_id: parent_id
+    }
+  end
+
+  ### Default Position
+
+  @doc """
+  This action fetches a list of all default positions for a document.
+
+  ## Arguments:
+
+    * `:client` - client to execute the HTTP request with
+    * `:document_type` - the document type that has the text positions
+    * `:document_id` - the id of the document with the text positions
+    * `:limit` - limit the number of results (default: 500, max: 2000)
+    * `:offset` - Skip over a number of elements by specifying an offset value for the query
+
+  """
+  @spec fetch_default_positions(
+          client :: Tesla.Client.t(),
+          document_type :: :offer | :order | :invoice,
+          document_id :: pos_integer(),
+          limit :: pos_integer() | nil,
+          offset :: non_neg_integer() | nil
+        ) :: {:ok, [BexioApiClient.SalesOrderManagement.PositionDefault.t()]} | {:error, any()}
+  def fetch_default_positions(
+        client,
+        document_type,
+        document_id,
+        limit \\ nil,
+        offset \\ nil
+      ) do
+    bexio_return_handling(
+      fn ->
+        Tesla.get(client, "/2.0/kb_#{document_type}/#{document_id}/kb_position_custom",
+          query: [limit: limit, offset: offset]
+        )
+      end,
+      &map_from_default_positions/1
+    )
+  end
+
+  @doc """
+  This action fetches a single default position for a document.
+
+  ## Arguments:
+
+    * `:client` - client to execute the HTTP request with
+    * `:document_type` - the document type that has the subtotal positions
+    * `:document_id` - the id of the document with the subtotal positions
+    * `:position_id` - the id of the position
+  """
+  @spec fetch_default_position(
+          client :: Tesla.Client.t(),
+          document_type :: :offer | :order | :invoice,
+          document_id :: pos_integer(),
+          position_id :: pos_integer()
+        ) :: {:ok, [BexioApiClient.SalesOrderManagement.PositionDefault.t()]} | {:error, any()}
+  def fetch_default_position(
+        client,
+        document_type,
+        document_id,
+        position_id
+      ) do
+    bexio_return_handling(
+      fn ->
+        Tesla.get(client, "/2.0/kb_#{document_type}/#{document_id}/kb_position_custom/#{position_id}")
+      end,
+      &map_from_default_position/1
+    )
+  end
+
+  defp map_from_default_positions(default_positions), do: Enum.map(default_positions, &map_from_default_position/1)
+
+  defp map_from_default_position(%{
+         "id" => id,
+         "amount" => amount,
+         "unit_id" => unit_id,
+         "account_id" => account_id,
+         "unit_name" => unit_name,
+         "tax_id" => tax_id,
+         "tax_value" => tax_value,
+         "text" => text,
+         "unit_price" => unit_price,
+         "discount_in_percent" => discount_in_percent,
+         "position_total" => position_total,
+         "pos" => pos,
+         "internal_pos" => internal_pos,
+         "is_optional" => optional?,
+         "parent_id" => parent_id
+       }) do
+    %PositionDefault{
+      id: id,
+      amount: Decimal.new(amount),
+      unit_id: unit_id,
+      account_id: account_id,
+      unit_name: unit_name,
+      tax_id: tax_id,
+      tax_value: Decimal.new(tax_value),
+      text: text,
+      unit_price: Decimal.new(unit_price),
+      discount_in_percent: Decimal.new(discount_in_percent),
+      position_total: Decimal.new(position_total),
       pos: pos,
       internal_pos: internal_pos,
       optional?: optional?,
---
+++ lib/bexio_api_client/sales_order_management/position_default.ex
@@ -0,0 +1,77 @@
+defmodule BexioApiClient.SalesOrderManagement.PositionDefault do
+  @moduledoc """
+  Default Position (KbPositionCustom)
+  """
+
+  @typedoc """
+  Default Position
+
+  ## Fields:
+
+    * `:id` - automatic id given by bexio
+    * `:text` - text
+    * `:amount` - amount
+    * `:account_id` - References an account object
+    * `:unit_id` - References a unit object
+    * `:unit_name` - Unit Name
+    * `:unit_price` - Unit Price
+    * `:discount_in_percent` - Discount in Percentage
+    * `:position_total` - Position Total
+    * `:tax_id` - References a tax object (only active sales taxes can be used)
+    * `:tax_value` - Tax Value
+    * `:pos` - Position
+    * `:optional?` - is the position optional?
+    * `:internal_pos` - Internal position id
+    * `:parent_id - if exists or nil
+
+  """
+  @type t :: %__MODULE__{
+          id: integer(),
+          amount: Decimal.t(),
+          unit_id: integer(),
+          account_id: integer(),
+          unit_name: String.t(),
+          tax_id: integer(),
+          tax_value: Decimal.t(),
+          text: String.t(),
+          unit_price: Decimal.t(),
+          discount_in_percent: Decimal.t(),
+          position_total: Decimal.t(),
+          internal_pos: integer(),
+          optional?: boolean(),
+          parent_id: integer() | nil
+        }
+  @enforce_keys [
+    :id,
+    :amount,
+    :unit_id,
+    :account_id,
+    :unit_name,
+    :tax_id,
+    :tax_value,
+    :text,
+    :unit_price,
+    :discount_in_percent,
+    :position_total,
+    :pos,
+    :internal_pos,
+    :optional?
+  ]
+  defstruct [
+    :id,
+    :amount,
+    :unit_id,
+    :account_id,
+    :unit_name,
+    :tax_id,
+    :tax_value,
+    :text,
+    :unit_price,
+    :discount_in_percent,
+    :position_total,
+    :pos,
+    :internal_pos,
+    :optional?,
+    :parent_id
+  ]
+end
