bfadc1f686745b229bb5b4987b710142089abdb2
Rico Metzger <rico.metzger@gmail.com>
2023-04-05 14:06:09 +0200

    Optimize

--- .credo.exs
+++ .credo.exs
@@ -13,8 +13,6 @@
       parse_timeout: 5000,
       color: true,
       checks: [
-        {Credo.Check.Design.AliasUsage, priority: :low},
-        # ... other checks omitted for readability ...
       ]
     }
   ]
